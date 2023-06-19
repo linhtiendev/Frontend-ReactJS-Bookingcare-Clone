0039d4dd08d1c7eb84762d22d0f78b6da697d207
linhtiendev <lelinhtien0707@gmail.com>
2023-06-19 16:33:04 +0700

    render user data to view (READ)

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,33 +1,87 @@
-import React, { Component } from 'react';
-import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import "./UserManage.scss";
+import { getAllUsers } from "../../services/userService";
 class UserManage extends Component {
-
-    state = {
-
+    //Khai báo theo chuẩn React
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: [],
+        };
     }
 
-    componentDidMount() {
-
+    // Goi API
+    async componentDidMount() {
+        let response = await getAllUsers("ALL");
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users,
+            });
+        }
     }
 
+    /**
+     * Life cycle (vòng đời)
+    run component:
+    1. Run constructer -> init state (biến muốn dùng)
+    2. Did mount (set state. Khi muốn gán giá trị trong biến state thì dùng)
+    - gọi API, set state
+    - state có nhiệm vụ lưu trữ các giá trị của các biến
+    3. Render
+    - dùng state trong render sẽ render cho cta thấy
+     */
+
+    //
 
     render() {
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+                <div className="title text-center">Manage Users</div>
+                <div className="user-table mt-3 mx-3">
+                    <table id="customers">
+                        <tr>
+                            <th>Email</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Address</th>
+                            <th>Actions</th>
+                        </tr>
+                        {arrUsers &&
+                            arrUsers.map((item, index) => {
+                                console.log("LITI check map", item, index);
+                                return (
+                                    <tr key={index}>
+                                        <td>{item.email}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.address}</td>
+                                        <td>
+                                            <button className="btn-edit">
+                                                <i className="fas fa-pencil-alt"></i>
+                                            </button>
+                                            <button className="btn-delete">
+                                                <i className="fas fa-trash"></i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                    </table>
+                </div>
+            </div>
         );
     }
-
 }
 
-const mapStateToProps = state => {
-    return {
-    };
+const mapStateToProps = (state) => {
+    return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+    return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
--- src/services/userService.js
+++ src/services/userService.js
@@ -11,4 +11,9 @@ const handleLoginApi = (email, password) => {
     return axios.post("api/login", { email, password });
 };
 
-export { handleLoginApi };
+const getAllUsers = (inputId) => {
+    //template string ``
+    return axios.get(`/api/get-all-users?id=${inputId}`);
+};
+
+export { handleLoginApi, getAllUsers };
