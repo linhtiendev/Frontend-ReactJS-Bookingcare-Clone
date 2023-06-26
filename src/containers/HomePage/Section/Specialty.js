184fc26e526874c4d9f09d7e12ac666f606c0583
linhtiendev <lelinhtien0707@gmail.com>
2023-06-26 15:13:46 +0700

    customize React-Slider-Carousel

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -10,36 +10,50 @@ import "slick-carousel/slick/slick-theme.css";
 class Specialty extends Component {
     render() {
         let settings = {
-            dots: true,
+            dots: false,
             infinite: true,
             speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1,
+            slidesToShow: 4,
+            slidesToScroll: 2, // scroll item
         };
         return (
             <React.Fragment>
-                <div className="section-specialty-container">
-                    <div className="specialty-content">
-                        <Slider {...settings}>
-                            <div className="img-customize">
-                                <h3>1</h3>
-                            </div>
-                            <div className="img-customize">
-                                <h3>2</h3>
-                            </div>
-                            <div className="img-customize">
-                                <h3>3</h3>
-                            </div>
-                            <div className="img-customize">
-                                <h3>4</h3>
-                            </div>
-                            <div className="img-customize">
-                                <h3>5</h3>
-                            </div>
-                            <div className="img-customize">
-                                <h3>6</h3>
-                            </div>
-                        </Slider>
+                <div className="section-specialty">
+                    <div className="specialty-container">
+                        <div className="specialty-header">
+                            <span className="title-section">
+                                Chuyên khoa phổ biến
+                            </span>
+                            <button className="btn-section">Xem thêm</button>
+                        </div>
+                        <div className="specialty-body">
+                            <Slider {...settings}>
+                                <div className="specialty-customize">
+                                    <div className="bg-image" />
+                                    <div>Khám tổng quát</div>
+                                </div>
+                                <div className="specialty-customize">
+                                    <div className="bg-image" />
+                                    <div>Khám tổng quát</div>
+                                </div>
+                                <div className="specialty-customize">
+                                    <div className="bg-image" />
+                                    <div>Khám tổng quát</div>
+                                </div>
+                                <div className="specialty-customize">
+                                    <div className="bg-image" />
+                                    <div>Khám tổng quát</div>
+                                </div>
+                                <div className="specialty-customize">
+                                    <div className="bg-image" />
+                                    <div>Khám tổng quát</div>
+                                </div>
+                                <div className="specialty-customize">
+                                    <div className="bg-image" />
+                                    <div>Khám tổng quát</div>
+                                </div>
+                            </Slider>
+                        </div>
                     </div>
                 </div>
             </React.Fragment>
